faac9c3af35f6b502731ffeda2e17bc0df8db1ef
WooCheol <jikimee64@gmail.com>
2021-12-11 20:58:18 +0900

    git init

---
+++ src/main/java/vendingmachine/Utils.java
@@ -0,0 +1,16 @@
+package vendingmachine;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Utils {
+
+    public void test(){
+
+//        Randoms.pickNumberInList()
+//
+//        Console.readLine();
+
+    }
+
+}
